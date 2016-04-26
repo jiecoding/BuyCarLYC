89a1a72be572dd893a1e647e46a43330a0e4392d
shangyijie <jiecoder@163.com>
2016-04-26 16:17:26 +0800

    Initial Commit

---
+++ BuyCarLYC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuyCarLYC
+//
+//  Created by laiyongche on 16/4/26.
+//  Copyright © 2016年 LYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BuyCarLYC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BuyCarLYC
+//
+//  Created by laiyongche on 16/4/26.
+//  Copyright © 2016年 LYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
